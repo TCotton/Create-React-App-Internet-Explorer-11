6a8bef9b62f87fe5169cbacb4741d8421802e766
Andy Walpole <andy.walpole@gmail.com>
2020-12-12 12:22:08 +0000

    added ie11 flexbox support

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,18 @@
-const { useBabelRc, override } = require('customize-cra')
+const { useBabelRc, override, addPostcssPlugins } = require('customize-cra')
 
 module.exports = override(
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    useBabelRc()
+    useBabelRc(),
+    addPostcssPlugins([
+        require("postcss-flexbugs-fixes"),
+        require("postcss-preset-env")({
+            autoprefixer: {
+                flexbox: "no-2009"
+            },
+            stage: 3,
+            features: {
+                "nesting-rules": true
+            }
+        })
+    ])
 );
